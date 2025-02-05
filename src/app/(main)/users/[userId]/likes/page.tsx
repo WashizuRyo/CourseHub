2d28bd744ba9b2929e562bd96bde147d00c9db2f
washizu <mokamikan1027@gmail.com>
2025-02-05 22:54:54 +0900

    feat: add user profile, likes, and reviews pages with data fetching

---
+++ src/app/(main)/users/[userId]/likes/page.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { useGetLikedReviewsAndCountByUserId } from '@/app/lib/users';
+import { useGetQueryParams } from '@/app/lib/users/functions';
+import SearchReviewSkeleton from '@/components/skeletons/search-review-skeleton';
+import ReviewTemplate from '@/components/universities/review-template';
+import ReviewSlector from '@/components/universities/userpage/review-selector';
+
+export default function Likes({ params }: { params: { userId: string } }) {
+  // ユーザIDを取得
+  console.log('test----------------------------');
+
+  const userId = params.userId;
+  // クエリパラメータのpageを取得
+  const { currentPage } = useGetQueryParams('page');
+
+  // ユーザがいいねしたレビューを取得
+  const { data, error, isError, isPending } = useGetLikedReviewsAndCountByUserId(userId, Number(currentPage));
+
+  // エラーが発生した場合
+  if (isError) {
+    return <div className="mt-3 text-center text-3xl font-bold">{error?.message}</div>;
+  }
+
+  // ローディング中
+  if (isPending) {
+    return (
+      <>
+        <ReviewSlector />
+        <div className="mt-3">
+          <SearchReviewSkeleton />
+        </div>
+      </>
+    );
+  }
+
+  // いいねしたレビューがなかった場合
+  if (!data)
+    return (
+      <>
+        <ReviewSlector />
+        <div className="m-4 text-center text-xl">いいねをするといいねしたレビューが表示されます</div>
+      </>
+    );
+
+  // いいねしたレビューを表示
+  return (
+    <>
+      <ReviewSlector />
+      <ReviewTemplate
+        userId={userId}
+        reviews={data.likedReviewByUserIdWithIsLikedTrue}
+        hitCount={data.likedReviewCountByUserId}
+      />
+    </>
+  );
+}
