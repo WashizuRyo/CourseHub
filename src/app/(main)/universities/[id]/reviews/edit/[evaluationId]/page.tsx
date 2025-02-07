3f234b952bb936b7e1f1fae1d6539f6e4abef038
washizu <mokamikan1027@gmail.com>
2025-02-07 22:45:02 +0900

    feat: add review edit page and not found component

---
+++ src/app/(main)/universities/[id]/reviews/edit/[evaluationId]/page.tsx
@@ -0,0 +1,26 @@
+import fetchReviewByEvaluationId from '@/app/lib/data'
+import UpdateReview from '@/components/universities/form/update-review-form'
+import NotAllowed from '@/components/universities/not-allowed'
+import { auth } from '@@/auth'
+import { SessionProvider } from 'next-auth/react'
+import { notFound } from 'next/navigation'
+
+export default async function Page({ params }: { params: { evaluationId: string; id: string } }) {
+  const review = await fetchReviewByEvaluationId(parseInt(params.evaluationId))
+  const session = await auth()
+  if (!review) {
+    notFound()
+  }
+
+  if (session?.user?.id !== review?.createdBy) {
+    return <NotAllowed />
+  }
+
+  return (
+    <div>
+      <SessionProvider session={session}>
+        <UpdateReview review={review} universityId={params.id} />
+      </SessionProvider>
+    </div>
+  )
+}
