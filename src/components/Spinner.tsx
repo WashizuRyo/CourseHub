a0bdededca713ccfd1a977ea011aff061ed57f14
washizu <mokamikan1027@gmail.com>
2025-02-08 17:16:44 +0900

    feat: add Spinner component for loading indication

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <div className='size-20 animate-spin rounded-full border-8 border-gray-300 border-t-blue-600' />
+    </div>
+  )
+}
