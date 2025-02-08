bd63a10eb83686706a1faea80a4a3c0f0eb46d67
washizu <mokamikan1027@gmail.com>
2025-02-08 13:05:55 +0900

    refactor: update user page layout and review selector for improved session handling and navigation

--- src/components/universities/userpage/review-selector.tsx
+++ src/components/universities/userpage/review-selector.tsx
@@ -1,22 +1,17 @@
-'use client';
+'use client'
 
-import clsx from 'clsx';
-import { useSession } from 'next-auth/react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import clsx from 'clsx'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
-export default function ReviewSlector() {
-  const { data: session } = useSession();
-  const pathname = usePathname();
-
-  // 自分が投稿したレビューページかどうか確認
-  const isReviewPage = pathname.split('/')[3].includes('reviews');
+export default function ReviewTabs({ userId }: { userId: string }) {
+  const pathname = usePathname()
+  const isReviewPage = pathname.split('/')[3].includes('reviews')
 
   return (
-    <div className="flex justify-center gap-5">
-      {/* 投稿したレビューへのリンク */}
+    <div className='flex justify-center gap-5 pb-6'>
       <Link
-        href={`/users/${session?.user?.id}/reviews`}
+        href={`/users/${userId}/reviews`}
         className={clsx(
           'rounded border p-2 hover:border-blue-400 hover:bg-sky-200',
           isReviewPage && 'border-blue-400 bg-sky-200',
@@ -25,9 +20,8 @@ export default function ReviewSlector() {
         投稿したレビュー
       </Link>
 
-      {/* いいねしたレビューへのリンク */}
       <Link
-        href={`/users/${session?.user?.id}/likes`}
+        href={`/users/${userId}/likes`}
         className={clsx(
           'rounded border p-2 hover:border-blue-400 hover:bg-sky-200',
           !isReviewPage && 'border-blue-400 bg-sky-200',
@@ -36,5 +30,5 @@ export default function ReviewSlector() {
         いいねしたレビュー
       </Link>
     </div>
-  );
+  )
 }
