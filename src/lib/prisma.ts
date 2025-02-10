0a37af9f0a9ce3cdf0f539c8cedbbb32ae3d639e
washizu <mokamikan1027@gmail.com>
2025-02-10 21:52:30 +0900

    refactor: simplify Prisma client initialization by removing logging configuration

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,11 +3,7 @@ import { PrismaClient } from '@prisma/client'
 // グローバル変数にPrismaクライアントを格納するための型定義
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 // Prismaクライアントインスタンスを作成または再利用
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'], // クエリのログを出力する設定（開発中のみ有効）
-  })
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 // 開発環境では、グローバル変数にPrismaクライアントインスタンスを設定
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
