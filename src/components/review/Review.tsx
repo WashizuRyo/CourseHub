e8ae9f20f10fc753ee11d2fd3f80885cdc3b233e
washizu <mokamikan1027@gmail.com>
2025-02-11 22:00:56 +0900

    fix: update import paths and rename components in Review

--- src/components/review/Review.tsx
+++ src/components/review/Review.tsx
@@ -1,5 +1,5 @@
-import { StarNumber } from '@/components/review/star-number'
-import DeleteDialog from '@/components/universities/delete-dialog'
+import DeleteDialog from '@/components/review/DeleteDialog'
+import { Star } from '@/components/review/Star'
 import Likes from '@/components/universities/likes'
 import { DefaultUserAvatar } from '@/components/user/default-user-avatar'
 import { UserAvatar } from '@/components/user/user-avatar'
@@ -12,7 +12,6 @@ export const Review = ({ review }: { review: ReviewWithMetadata }) => {
   return (
     <div key={review.id} className='p-1'>
       <div className='rounded-md bg-white p-2'>
-        {/* ユーザ画像表示 */}
         <div className='flex'>
           {review.isAnonymous ? (
             <>
@@ -27,44 +26,30 @@ export const Review = ({ review }: { review: ReviewWithMetadata }) => {
           )}
         </div>
 
-        {/* 学部名 */}
         <div className='mt-3'>{review.faculty}</div>
-
-        {/* 授業名 */}
         <div className='mt-2 text-xl'>{review.className}</div>
-
-        {/* スマホから閲覧した場合のレビュータイトル */}
         <div className='mb-1 mt-2 break-all md:hidden'>
           <p className='text-xl'>{review.title}</p>
         </div>
 
         <div className='mb-2'>
           <div className='flex'>
-            {/* 星の数 */}
-            <StarNumber starNumber={review.star} />
-
+            <Star starNumber={review.star} />
             {/* パソコンから閲覧した場合のレビュータイトル */}
             <div className='hidden md:ml-2 md:flex md:items-center'>
               <p className='text-2xl'>{review.title}</p>
             </div>
           </div>
-
-          {/* レビューした日 */}
           <p className='my-1 text-gray-400'>{review.date}にレビュー</p>
-
-          {/* レビュー内容 */}
           <p className='w-128 whitespace-break-spaces break-all leading-7'>{review.evaluation}</p>
         </div>
-
         <div className='flex justify-between'>
-          {/* いいね機能 */}
           <div className='flex items-end'>
             <div className='flex flex-col'>
               <Likes reviewId={review.id} isLiked={review.isLiked} />
             </div>
           </div>
 
-          {/* 編集と削除ページへのリンク */}
           {review.isAuthor && (
             <div className='mt-6 flex justify-end gap-2'>
               <div className='flex flex-col items-center'>
@@ -74,7 +59,7 @@ export const Review = ({ review }: { review: ReviewWithMetadata }) => {
                   </Link>
                 </div>
               </div>
-              <DeleteDialog universityId={review.universityId} evaluationId={review.id} query={review.className} />
+              <DeleteDialog reviewId={review.id} />
             </div>
           )}
         </div>
