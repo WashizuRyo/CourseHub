b3ab56e1513cd3bb6f749d2d2791c2343bfae60d
washizu <mokamikan1027@gmail.com>
2025-02-11 16:40:21 +0900

    fix: change  review components dir and university search functionality

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,14 +1,14 @@
-import { Inter, Lusitana, Noto_Sans_JP, Noto_Serif_JP } from 'next/font/google';
+import { Inter, Lusitana, Noto_Sans_JP, Noto_Serif_JP } from 'next/font/google'
 
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] })
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
-});
+})
 export const notosans = Noto_Sans_JP({
   subsets: ['latin'],
-});
+})
 export const notoserif = Noto_Serif_JP({
   weight: ['400', '700'],
   subsets: ['latin'],
-});
+})
