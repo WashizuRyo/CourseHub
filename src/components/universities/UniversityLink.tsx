99e83c8a32a1e7b9915ca180783bb1394d21f9d0
washizu <mokamikan1027@gmail.com>
2025-02-11 23:00:04 +0900

    feat: create UniversityLink component and update page to use it

--- src/components/universities/UniversityLink.tsx
+++ src/components/universities/UniversityLink.tsx
@@ -1,23 +1,26 @@
-import { fetchUniversityByName } from '@/lib/data'
+import UniversitySearchBarSkeleton from '@/components/skeletons/UniversitySearchBarSkeleton'
+import { fetchUniversityByName } from '@/model/university'
 import { BuildingLibraryIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import { Suspense } from 'react'
 import NotFound from '../utils/NotFound'
 
-export default async function UniversityLinkContainer({ universityName }: { universityName: string }) {
-  // 大学が存在するかチェック
-  const university = await fetchUniversityByName(universityName)
+export default async function UniversityLink({ universityName }: { universityName: string }) {
+  return (
+    <Suspense key={universityName} fallback={<UniversitySearchBarSkeleton />}>
+      <UniversityFetcher universityName={universityName} />
+    </Suspense>
+  )
+}
 
+async function UniversityFetcher({ universityName }: { universityName: string }) {
+  const university = await fetchUniversityByName(universityName)
   if (!university) {
     return <NotFound query={universityName} />
   }
 
-  return <UniversityLinkPresentation university={university} />
-}
-
-function UniversityLinkPresentation({ university }: { university: { universityId: number; universityname: string } }) {
   return (
     <div>
-      {/* 大学が見つかった場合はリンクを表示 */}
       <div className='flex justify-center'>
         <div className='flex justify-start rounded-xl border bg-blue-500 shadow-md hover:bg-blue-400'>
           <Link href={`/universities/${university?.universityId}`} className='flex items-center'>
