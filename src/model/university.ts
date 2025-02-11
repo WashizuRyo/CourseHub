aa61101ebdc3d6946c93178289a69b4f1517757a
washizu <mokamikan1027@gmail.com>
2025-02-11 23:00:20 +0900

    feat: refactor university data fetching functions and rename parameters

--- src/model/university.ts
+++ src/model/university.ts
@@ -1,11 +1,22 @@
 import { prisma } from '@/lib/prisma'
 
-export async function fetchUniversity(id: number) {
+export async function fetchUniversity(universityId: number) {
   try {
     const data = await prisma.university.findUnique({
-      where: { universityId: id },
+      where: { universityId },
     })
     return data
+  } catch (error) {
+    console.error('Database Error', error)
+    throw new Error('Failed to fetch university')
+  }
+}
+
+export async function fetchUniversityByName(universityname: string) {
+  try {
+    return await prisma.university.findUnique({
+      where: { universityname },
+    })
   } catch (error) {
     console.error('Database Error', error)
     throw new Error('Failed to fetch university')
